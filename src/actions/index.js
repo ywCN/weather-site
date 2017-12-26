10067ac81efb0797b3606aad6d981da6be72c9b5
ywCN <wangyouhao90@gmail.com>
2017-12-26 11:33:12 -0500

    comment about this action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,20 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
+/**
+ * This action creator is responsible for creating an action
+ * that contains a request to the backend API.
+ * 
+ * This action creator takes a city(string) as parameter, and
+ * use it as part of the search query.
+ * 
+ * The axios library is essentially behaves exactly like the 
+ * JQuery method which reaches out and does an AJAX request
+ * in the form of getting to the URL and returning a Promise.
+ * 
+ * This Promise will be passed to the action as payload.
+ * This action type is FETCH_WEATHER.
+ */
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
