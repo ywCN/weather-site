85f111dfd7740e3493994a8cf4b62e0fed17bb6b
ywCN <wangyouhao90@gmail.com>
2017-12-26 16:02:06 -0500

    applied chart

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,35 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
-        console.log('data:', name);
+        const temps = cityData.list.map(weather => weather.main.temp);
+
         return (
-            <tr key={name}>
-                <td>{name}</td>
-            </tr>
+        <tr key={name}>
+            <td>{name}</td>
+            <td>
+            <Sparklines height={120} width={180} data={temps}>
+                <SparklinesLine color="red" />
+            </Sparklines>
+            </td>
+        </tr>
         );
     }
 
     render() {
-        console.log('state:', this.props.weather);
         return (
-            <table className="table table-hover">
-            {/* thead is table head */}
-                <thead>
-                    <tr>
-                        <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* for each city, do this renderWeather callback function */}
-                    {this.props.weather.map(this.renderWeather)}
-                </tbody>
-            </table>
+        <table className="table table-hover">
+            <thead>
+            <tr>
+                <th>City</th>
+                <th>Temperature</th>
+                <th>Pressure</th>
+                <th>Humidity</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.props.weather.map(this.renderWeather)}
+            </tbody>
+        </table>
         );
     }
 }
