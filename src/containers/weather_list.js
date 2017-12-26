dac50a682de6bf9dc6683a4b57fb851056406e80
ywCN <wangyouhao90@gmail.com>
2017-12-26 17:35:58 -0500

    add style

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,6 +5,7 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
+        // can convert unit here
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
