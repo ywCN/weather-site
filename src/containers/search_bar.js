014b4223ee8bac5dbecec43e94eacb33c595ae8d
ywCN <wangyouhao90@gmail.com>
2017-12-25 21:10:16 -0500

    comment

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 export default class SearchBar extends Component {
     render() {
         return (
+            // styles are from bootstrap
             <form className="input-group">
                 <input />
                 <span className="input-group-btn">
