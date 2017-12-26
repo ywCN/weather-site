eb4388137b9fe92c03f14fc0b3decb18ed8188ce
ywCN <wangyouhao90@gmail.com>
2017-12-25 21:19:25 -0500

    this version won't display text

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -6,11 +6,20 @@ export default class SearchBar extends Component {
         this.state = { term: '' };
     }
 
+    onInputChange(event) {
+        console.log(event.target.value); // event.target.value is from JavaScript
+    }
+
     render() {
         return (
             // styles are from bootstrap
             <form className="input-group">
-                <input />
+                <input 
+                    placeholder="Get a 5 day forcast in your favorite city"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
                 <span className="input-group-btn">
                 {/* span is used to wrap a button */}
                     <button type="submit" className="btn btn-secondary">Submit</button>
