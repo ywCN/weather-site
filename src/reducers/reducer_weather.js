f8349110a6bc3e3eed6dfd949f91283e7f92819a
ywCN <wangyouhao90@gmail.com>
2017-12-26 14:26:21 -0500

    build up reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,12 @@
-export default function(state = null, action) {
+import FETCH_WEATHER from '../actions/index';
+
+export default function(state = [], action) {
     // console.log('Action received:', action);
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // action.payload.data is from the Object structure
+            // return state.concat([action.payload.data]);
+            return [ action.payload.data, ...state ]; // ES6
+    }
     return state;
 }
