2bd8bc5678837ec9419eb7f2f51f6baa4ebd86ef
ywCN <wangyouhao90@gmail.com>
2017-12-26 13:58:38 -0500

    add log for testing

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,6 +22,9 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
+
+    console.log('Request:', request);
+
     return {
         type: FETCH_WEATHER,
         payload: request
