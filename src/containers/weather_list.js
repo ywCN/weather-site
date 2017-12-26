164b511979000c72331c39a230e9b25c8552239c
ywCN <wangyouhao90@gmail.com>
2017-12-26 14:37:32 -0500

    build header

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -8,10 +8,13 @@ export default class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
                     </tr>
                 </thead>
                 <tbody>
-
+                    
                 </tbody>
             </table>
         );
