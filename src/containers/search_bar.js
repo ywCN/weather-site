9cae6da5c4dc250fcb635c290c0cc3182fa970bb
ywCN <wangyouhao90@gmail.com>
2017-12-25 21:09:21 -0500

    add button

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -5,6 +5,10 @@ export default class SearchBar extends Component {
         return (
             <form className="input-group">
                 <input />
+                <span className="input-group-btn">
+                {/* span is used to wrap a button */}
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
             </form>
         );
     }
