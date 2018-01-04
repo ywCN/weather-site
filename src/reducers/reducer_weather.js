3372741913f0453a66eb04ba2d62350ac59f33b9
ywCN <wangyouhao90@gmail.com>
2018-01-04 00:52:03 -0500

    handled bad input, but this approach is bad because it happens in reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,12 +1,16 @@
-import { FETCH_WEATHER } from '../actions/index';// Note: must use {}
+import { FETCH_WEATHER } from '../actions/index'; // Note: must use {}
 
 export default function(state = [], action) {
     // console.log('Action received:', action);
     switch (action.type) {
         case FETCH_WEATHER:
+            console.log('Request is:', action.payload.data);
+            if (!action.payload.data) {
+                return state;
+            }
             // action.payload.data is from the Object structure
             // return state.concat([action.payload.data]);
-            return [ action.payload.data, ...state ]; // ES6
+            return [action.payload.data, ...state]; // ES6
     }
     return state;
 }
